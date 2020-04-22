2a35ddc8cb77eb413e3fc389fdd6fe5b6f7316cc
chenpb-lu <48192388+chenpb-lu@users.noreply.github.com>
2020-04-22 11:01:22 +0800

    Update cpustat.go

--- cpustat.go
+++ cpustat.go
@@ -6,7 +6,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/gaochao1/gosnmp"
+	"https://github.com/chenpb-lu/gosnmp"
 )
 
 func CpuUtilization(ip, community string, timeout, retry int) (int, error) {
